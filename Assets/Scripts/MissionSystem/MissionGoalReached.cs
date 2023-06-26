91a9affb33218acaed08eb38302986cd7412f66f
kokoy1227 <133103325+kokoy1227@users.noreply.github.com>
2023-06-26 18:44:24 +0800

    update26

--- Assets/Scripts/MissionSystem/MissionGoalReached.cs
+++ Assets/Scripts/MissionSystem/MissionGoalReached.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MissionGoalReached : MonoBehaviour
 {
@@ -9,13 +8,18 @@ public class MissionGoalReached : MonoBehaviour
     public Player _player;
     public Mission _mission;
 
+    public GameObject Panels;
     public GameObject _missionWindow;
+    public Timelines _timelines;
 
     public void OnTriggerEnter(Collider other){
        _mission.goalLocation.SetActive(false);
         _mission.goalWaypoint.SetActive(false);
         _missionWindow.SetActive(false);
-        SceneManager.LoadScene(5);
+        Panels.SetActive(false);
+        _timelines.timeline1.SetActive(true);
+
+        
 
     }
 }
--- Assets/Scripts/Timelines/TimelineManager.cs
+++ Assets/Scripts/Timelines/TimelineManager.cs
@@ -7,12 +7,15 @@ public class TimelineManager : MonoBehaviour
     public float changeTime;
     public GameObject timeline;
 
+    public GameObject _panels;
+
     private void Update()
     {
         changeTime -= Time.deltaTime;
         if(changeTime <= 0)
         {
             timeline.SetActive(false);
+            _panels.SetActive(true);
         }
     }
 }
